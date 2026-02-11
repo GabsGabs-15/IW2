6233826536dedf667f5972616250e5164765d425
GabsGabs-15 <ggdenot150708@gmail.com>
2026-02-11 15:29:40 -0300

    Add files via upload

---
+++ IW2/script.js
@@ -0,0 +1,28 @@
+//Texto
+let nome = "Gabriel";
+let nome2 = "Filip";
+
+console.log("Meu nome é: " + nome);
+console.log(nome2);
+
+//Números
+let num = 17;
+console.log("eu tenho " + num + " anos");
+
+//Booleanos
+
+let maiorDe18Anos = true;
+console.log(maiorDe18Anos);
+
+//null e undefined
+
+let testeNull = null;
+let testeUndefined;
+
+console.log(testeNull);
+console.log(testeUndefined);
+console.log(testeUndefined * 2);
+console.log("6" + 4);
+console.log("6" - 4);
+console.log("6" * 4);
+console.log("6" / 4);
