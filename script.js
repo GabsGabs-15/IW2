bab6d697ece004d1e2ba6c4f3ac6e001f59bf5f6
GabsGabs-15 <ggdenot150708@gmail.com>
2026-02-24 21:13:22 +0000

    Aula do dia 24/02

--- script.js
+++ script.js
@@ -17,6 +17,13 @@ console.log(maiorDe18Anos);
 //null e undefined
 
 let testeNull = null;
+let numero2 = 3
+if (testeNull != null) {
+    console.log("não é nulo!");
+}
+else{
+    console.log("É nulo")
+}
 let testeUndefined;
 
 console.log(testeNull);
@@ -26,3 +33,35 @@ console.log("6" + 4);
 console.log("6" - 4);
 console.log("6" * 4);
 console.log("6" / 4);
+
+let numero = 1;
+let numeroString = String(numero);
+let string = "86";
+let stringNumero = Number(string);
+
+console.log(typeof numero, numero);
+console.log(typeof numeroString, numeroString);
+console.log(typeof string, string);
+console.log(typeof stringNumero, stringNumero);
+
+//Tipos de variaveis em JS
+
+//let
+
+let teste = 2;
+teste = 4;
+
+if (teste == 4){
+    console.log(teste);
+}
+
+//Operadores
+
+let contador = 0;
+contador++; //Pega o valor da variael contador e adiciona +1
+contador=+ 3; // Pega o valor de contador e adiciona o valor especificado
+console.log (contador += 3);
+
+//>=, <=, ||, &&, ==, ===
+//== identifica apenas o valor
+//=== identifica o valor e o tipo
